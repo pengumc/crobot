9f0e91749730be828e8b2d3b1c58652f323a8730
Michiel van der Coelen <m.vandercoelen@gmail.com>
2012-03-25 14:25:16 +0200

    added copyright

--- src/template.c
+++ src/template.c
@@ -16,23 +16,3 @@
     along with Crobot.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
-#define __MAIN__
-#include <stdio.h>
-#include "Usbdevice.h"
-#include "Report.h"
-
-int main(int argc, char* argv[]){
-    int plop;
-    printf("pointer size = %d bytes\n", sizeof(&plop));
-    printf("unsigned char = %d\n", sizeof(unsigned char));
-    printf("int size = %d\n", sizeof(int));
-    usbdevice_t* myDev = Usbdevice_alloc();
-    printf("sizeof usbdev = %d\n", sizeof(usbdevice_t));
-    printf("%s\n", myDev->vendor);
-    printf("dev connect: %d\n", Usbdevice_connect(myDev));
-    Usbdevice_free(myDev);
-
-    return(0);
-}
-
